8249db51ab8c39a675ef88bf7c7caf282f373fe2
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-14 16:08:46 +0100

    Common: Improved control context

--- src/source/SourceControlProvider.tsx
+++ src/source/SourceControlProvider.tsx
@@ -4,11 +4,39 @@ import {PropsWithChildren} from "react";
 export interface ISourceControlProviderProps<TFilter = any, TOrderBy = any> {
 	filterProviderProps?: IFilterProviderProps<TFilter>;
 	orderByProviderProps?: IOrderByProviderProps<TOrderBy>;
+
+	/**
+	 * Default pre-set filter; could be overridden by a user. Apply filter prop takes precedence.
+	 */
+	defaultFilter?: TFilter;
+	/**
+	 * Apply the given filter all the times (regardless of values set by a user)
+	 */
+	applyFilter?: TFilter;
+	/**
+	 * Default pre-set order; could be overridden by a user. Apply filter prop takes precedence.
+	 */
+	defaultOrderBy?: TOrderBy;
 }
 
-export function SourceControlProvider<TFilter = any, TOrderBy = any>({filterProviderProps, orderByProviderProps, children}: PropsWithChildren<ISourceControlProviderProps<TFilter, TOrderBy>>) {
-	return <FilterProvider<TFilter> {...filterProviderProps}>
-		<OrderByProvider<TOrderBy> {...orderByProviderProps}>
+export function SourceControlProvider<TFilter = any, TOrderBy = any>(
+	{
+		filterProviderProps,
+		orderByProviderProps,
+		defaultFilter,
+		applyFilter,
+		defaultOrderBy,
+		children,
+	}: PropsWithChildren<ISourceControlProviderProps<TFilter, TOrderBy>>) {
+	return <FilterProvider<TFilter>
+		defaultFilter={defaultFilter}
+		applyFilter={applyFilter}
+		{...filterProviderProps}
+	>
+		<OrderByProvider<TOrderBy>
+			defaultOrderBy={defaultOrderBy}
+			{...orderByProviderProps}
+		>
 			{children}
 		</OrderByProvider>
 	</FilterProvider>
