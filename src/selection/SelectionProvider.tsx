8f76f3c8e8b4e0d8eb06ad04a686ee3a53227f00
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-17 20:15:08 +0200

    Common: Version bump

---
+++ src/selection/SelectionProvider.tsx
@@ -0,0 +1,54 @@
+import {ISelectionBoolType, ISelectionType, SelectionBoolTypeMap} from "@leight-core/api";
+import {SelectionContext} from "@leight-core/client";
+import {PropsWithChildren, useEffect, useState} from "react";
+
+export interface ISelectionProviderProps<TSelection = any> {
+	/**
+	 * Selection type.
+	 */
+	type?: ISelectionType;
+	/**
+	 * Bool type cycle.
+	 */
+	bool?: ISelectionBoolType;
+	/**
+	 * Default pre-set selection; could be overridden by a user. Apply selection prop takes precedence.
+	 */
+	defaultSelection?: { [index in string]: TSelection };
+	/**
+	 * Apply the given selection all the times (regardless of values set by a user)
+	 */
+	applySelection?: { [index in string]: TSelection };
+}
+
+export function SelectionProvider<TSelection, >({type, bool = "true/false/undefined", defaultSelection, applySelection, ...props}: PropsWithChildren<ISelectionProviderProps<TSelection>>) {
+	const [selection, setSelection] = useState<{ [index in string]: TSelection | undefined }>(applySelection || defaultSelection || {});
+	useEffect(() => {
+		setSelection(defaultSelection || {});
+	}, [defaultSelection]);
+	useEffect(() => {
+		setSelection(applySelection || {});
+	}, [applySelection]);
+	return <SelectionContext.Provider
+		value={{
+			isSelected: id => !!selection[id],
+			asSelection: () => selection,
+			toSelection: () => Object.keys(selection).filter(key => !!selection[key]),
+			onSelect: (id, selection) => {
+				setSelection(prev => {
+					const _bool = SelectionBoolTypeMap[bool][`${prev[id]}`];
+					switch (type) {
+						case "none":
+							return {};
+						case "single":
+							return {[id]: _bool ? selection : undefined};
+						case "multi":
+							return {...prev, [id]: _bool ? selection : undefined};
+					}
+					return {};
+				});
+			},
+		}}
+		{...props}
+	/>;
+}
