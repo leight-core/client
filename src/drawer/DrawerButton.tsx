fca818cbe05e6260990812dec4ab1e81a5ebcdec
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-18 00:59:15 +0200

    Common: Version bump

--- src/drawer/DrawerButton.tsx
+++ src/drawer/DrawerButton.tsx
@@ -1,6 +1,6 @@
 import {PlacementType} from "@leight-core/api";
 import {Drawer, DrawerContext, DrawerProvider, isString, useMobile} from "@leight-core/client";
-import {Button, ButtonProps, DrawerProps} from "antd";
+import {Button, ButtonProps, DrawerProps, Tooltip} from "antd";
 import {PushState} from "antd/lib/drawer";
 import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
@@ -9,6 +9,7 @@ export interface IDrawerButtonProps extends Partial<ButtonProps> {
 	label?: ReactNode | string;
 	title?: string;
 	values?: any;
+	tooltip?: string;
 	/**
 	 * Optional drawer width.
 	 */
@@ -23,35 +24,37 @@ export interface IDrawerButtonProps extends Partial<ButtonProps> {
 /**
  * Default Antd button without any preset; just the drawer is shown on click.
  */
-export const DrawerButton: FC<IDrawerButtonProps> = ({children, onClick, label, title, values, width = 600, height, placement = "right", push = false, fullscreen = false, drawerProps, ...props}) => {
+export const DrawerButton: FC<IDrawerButtonProps> = ({children, onClick, label, title, tooltip, values, width = 600, height, placement = "right", push = false, fullscreen = false, drawerProps, ...props}) => {
 	const {t} = useTranslation();
 	const mobile = useMobile();
 	fullscreen && (width = "100vw") && (height = "100vh");
-	return <DrawerProvider>
-		<DrawerContext.Consumer>
-			{drawerContext => <>
-				<Drawer
-					title={title ? t(title, {data: values}) : null}
-					width={mobile("100vw", width)}
-					height={height}
-					headerStyle={mobile({padding: "8px 4px"})}
-					bodyStyle={{overflowY: "scroll", padding: mobile("4px")}}
-					placement={placement}
-					push={push}
-					{...drawerProps}
-				>
-					{children}
-				</Drawer>
-				<Button
-					onClick={event => {
-						drawerContext.setVisible(true);
-						onClick?.(event);
-					}}
-					{...props}
-				>
-					{isString(label) ? t(label as string, {data: values}) : label}
-				</Button>
-			</>}
-		</DrawerContext.Consumer>
-	</DrawerProvider>;
+	return <Tooltip title={tooltip ? t(tooltip) : undefined}>
+		<DrawerProvider>
+			<DrawerContext.Consumer>
+				{drawerContext => <>
+					<Drawer
+						title={title ? t(title, {data: values}) : null}
+						width={mobile("100vw", width)}
+						height={height}
+						headerStyle={mobile({padding: "8px 4px"})}
+						bodyStyle={{overflowY: "scroll", padding: mobile("4px")}}
+						placement={placement}
+						push={push}
+						{...drawerProps}
+					>
+						{children}
+					</Drawer>
+					<Button
+						onClick={event => {
+							drawerContext.setVisible(true);
+							onClick?.(event);
+						}}
+						{...props}
+					>
+						{isString(label) ? t(label as string, {data: values}) : label}
+					</Button>
+				</>}
+			</DrawerContext.Consumer>
+		</DrawerProvider>
+	</Tooltip>;
 };
