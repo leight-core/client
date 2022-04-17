90564fe3b4ba44996ff6d936cd6a046c2f2363c7
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-18 00:50:53 +0200

    Common: Version bump

--- src/component/BoolInline.tsx
+++ src/component/BoolInline.tsx
@@ -1,20 +1,24 @@
-import {CheckCircleTwoTone, CloseCircleTwoTone, MinusCircleTwoTone, TwoToneColor} from "@ant-design/icons";
+import {CheckCircleTwoTone, CloseCircleTwoTone, MinusCircleTwoTone} from "@ant-design/icons";
 import {Avatar} from "antd";
-import {FC} from "react";
+import {FC, ReactNode} from "react";
 
 export interface IBoolInlineProps {
 	bool?: boolean | null;
-	trueColor?: TwoToneColor;
-	falseColor?: TwoToneColor;
-	unknownColor?: TwoToneColor;
+	checkIcon?: ReactNode;
+	uncheckIcon?: ReactNode;
+	undefinedIcon?: ReactNode;
 }
 
-export const BoolInline: FC<IBoolInlineProps> = ({bool, trueColor = "#71da17", falseColor = "#e26738", unknownColor}) => {
+export const BoolInline: FC<IBoolInlineProps> = (
+	{
+		bool,
+		checkIcon = <CheckCircleTwoTone twoToneColor={"#71da17"}/>,
+		uncheckIcon = <CloseCircleTwoTone twoToneColor={"#e26738"}/>,
+		undefinedIcon = <MinusCircleTwoTone/>,
+	}) => {
 	return <Avatar
 		size={"large"}
 		style={{backgroundColor: "transparent"}}
-		icon={bool !== undefined && bool !== null ?
-			(bool ? <CheckCircleTwoTone twoToneColor={trueColor}/> : <CloseCircleTwoTone twoToneColor={falseColor}/>) :
-			<MinusCircleTwoTone twoToneColor={unknownColor}/>
-		}/>;
+		icon={bool !== undefined && bool !== null ? (bool ? checkIcon : uncheckIcon) : undefinedIcon}
+	/>;
 };
