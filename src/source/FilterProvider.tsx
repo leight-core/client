5962fe0b015a288bfc526c347366827f92e5f6bf
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-15 02:19:33 +0200

    Common: Version bump

--- src/source/FilterProvider.tsx
+++ src/source/FilterProvider.tsx
@@ -3,12 +3,6 @@ import deepmerge from "deepmerge";
 import empty from "is-empty";
 import {PropsWithChildren, useEffect, useState} from "react";
 
-interface IFilterState {
-	filter: any;
-	request: any;
-	source: any;
-}
-
 export type IFilterProviderProps<TFilter = any> = PropsWithChildren<{
 	name: string;
 	/**
@@ -22,32 +16,25 @@ export type IFilterProviderProps<TFilter = any> = PropsWithChildren<{
 }>;
 
 export function FilterProvider<TFilter, >({name, defaultFilter, applyFilter, ...props}: IFilterProviderProps<TFilter>) {
-	const $key = `${name}.filter`;
-	const state: IFilterState = JSON.parse(window.localStorage.getItem($key) || "{}");
 	/**
 	 * Currently set filter; applied with defaults/applied.
 	 */
-	const [filter, setFilter] = useState<TFilter | undefined>(deepmerge(state?.filter || {}, applyFilter || defaultFilter || {}));
+	const [filter, setFilter] = useState<TFilter | undefined>(applyFilter || defaultFilter);
 	/**
 	 * Filter set by the user; this is useful to distinguish isEmpty() which could contain applied filters which
 	 * should not be visible by the user
 	 */
-	const [request, setRequest] = useState<TFilter | undefined>(state?.request);
+	const [request, setRequest] = useState<TFilter | undefined>();
 	/**
 	 * When used in a form, for example, this is the source used to build-up this filter.
 	 */
-	const [source, setSource] = useState(state?.source);
+	const [source, setSource] = useState();
 
 	const $setFilter = (value?: TFilter, request?: TFilter, source?: any) => {
 		const filter = rundef(value);
 		setFilter(empty(filter) ? undefined : filter);
 		setRequest(request);
 		setSource(source);
-		window.localStorage.setItem($key, JSON.stringify({
-			filter,
-			request,
-			source,
-		}));
 	};
 
 	useEffect(() => {
