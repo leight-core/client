347ff1e7d4bb91586c37ff960fe1e8a861eb9bfb
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-15 02:08:12 +0200

    Common: Version bump

--- src/source/FilterProvider.tsx
+++ src/source/FilterProvider.tsx
@@ -3,6 +3,12 @@ import deepmerge from "deepmerge";
 import empty from "is-empty";
 import {PropsWithChildren, useEffect, useState} from "react";
 
+interface IFilterState {
+	filter: any;
+	request: any;
+	source: any;
+}
+
 export type IFilterProviderProps<TFilter = any> = PropsWithChildren<{
 	name: string;
 	/**
@@ -16,24 +22,32 @@ export type IFilterProviderProps<TFilter = any> = PropsWithChildren<{
 }>;
 
 export function FilterProvider<TFilter, >({name, defaultFilter, applyFilter, ...props}: IFilterProviderProps<TFilter>) {
+	const $key = `${name}.filter`;
+	const state: IFilterState = JSON.parse(window.localStorage.getItem($key) || "{}");
 	/**
 	 * Currently set filter; applied with defaults/applied.
 	 */
-	const [filter, setFilter] = useState<TFilter | undefined>(applyFilter || defaultFilter);
+	const [filter, setFilter] = useState<TFilter | undefined>(deepmerge(state?.filter, applyFilter || defaultFilter));
 	/**
 	 * Filter set by the user; this is useful to distinguish isEmpty() which could contain applied filters which
 	 * should not be visible by the user
 	 */
-	const [request, setRequest] = useState<TFilter | undefined>();
+	const [request, setRequest] = useState<TFilter | undefined>(state?.request);
 	/**
 	 * When used in a form, for example, this is the source used to build-up this filter.
 	 */
-	const [source, setSource] = useState();
+	const [source, setSource] = useState(state?.source);
 
-	const $setFilter = (value?: TFilter, source?: any) => {
+	const $setFilter = (value?: TFilter, request?: TFilter, source?: any,) => {
 		const filter = rundef(value);
 		setFilter(empty(filter) ? undefined : filter);
+		setRequest(request);
 		setSource(source);
+		window.localStorage.setItem($key, JSON.stringify({
+			filter,
+			request,
+			source,
+		}));
 	};
 
 	useEffect(() => {
@@ -47,18 +61,9 @@ export function FilterProvider<TFilter, >({name, defaultFilter, applyFilter, ...
 			name,
 			filter,
 			source,
-			setFilter: (filter, source) => {
-				setRequest(filter);
-				$setFilter({...filter, ...applyFilter}, source);
-			},
-			applyFilter: apply => {
-				setRequest(apply);
-				$setFilter({...filter, ...apply, ...applyFilter});
-			},
-			mergeFilter: apply => {
-				setRequest(apply);
-				$setFilter(deepmerge(deepmerge<TFilter, TFilter>(filter || {}, apply), applyFilter));
-			},
+			setFilter: (filter, source) => $setFilter({...filter, ...applyFilter}, filter, source),
+			applyFilter: apply => $setFilter({...filter, ...apply, ...applyFilter}, apply),
+			mergeFilter: apply => $setFilter(deepmerge(deepmerge<TFilter, TFilter>(filter || {}, apply), applyFilter), apply),
 			isEmpty: () => empty(request),
 		}}
 		{...props}
