a3de1a5955e234a80a8aaa87aa5c07f68da25286
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-25 00:31:22 +0200

    Common: Version bump

--- src/user/UserProvider.tsx
+++ src/user/UserProvider.tsx
@@ -1,18 +1,24 @@
 import {IUserRequest} from "@leight-core/api";
-import {User, UserContext} from "@leight-core/client";
+import {LoaderIcon, Template, User, UserContext} from "@leight-core/client";
 import {FC, PropsWithChildren} from "react";
 
 export type IUserProviderProps = PropsWithChildren<{
 	user: IUserRequest;
 	isReady: boolean;
+	block?: boolean;
 }>
 
-export const UserProvider: FC<IUserProviderProps> = ({user, isReady, ...props}) => {
+export const UserProvider: FC<IUserProviderProps> = ({user, isReady, block = true, children, ...props}) => {
+	children = isReady || !block ? children : <Template
+		extra={<LoaderIcon/>}
+	/>;
 	return <UserContext.Provider
 		value={{
 			user: User(user),
 			isReady
 		}}
 		{...props}
-	/>;
+	>
+		{children}
+	</UserContext.Provider>;
 };
