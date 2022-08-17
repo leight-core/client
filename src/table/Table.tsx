4ca0a1f5594e3bbcd76f50431bf92bcb9ac7d731
Marek Hanzal <marek.hanzal@x32.cz>
2022-08-17 15:23:15 +0200

    Common: Version bump

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -1,37 +1,43 @@
-import {ISourceContext, ISourceItem, ITableChildrenCallback} from "@leight-core/api";
 import {LoaderIcon, PaginationContext, PaginationProvider, Template, useSourceContext} from "@leight-core/client";
 import {isCallable, isString} from "@leight-core/utils";
 import {UseQueryResult} from "@tanstack/react-query";
 import {Empty, SpinProps, Table as CoolTable, TableProps} from "antd";
+import type {ColumnProps} from "antd/es/table";
 import React, {ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 
-export interface ITableProps<TSourceContext extends ISourceContext<any>> extends Omit<TableProps<ISourceItem<TSourceContext>>, "header" | "footer" | "children"> {
-	header?: (sourceContext: TSourceContext) => ReactNode;
-	footer?: (sourceContext: TSourceContext) => ReactNode;
-	children?: ITableChildrenCallback<TSourceContext> | ReactNode;
+export interface ITableColumnProps<TItem> extends Omit<ColumnProps<TItem>, "dataIndex"> {
+	readonly dataIndex?: keyof TItem;
+}
+
+export interface IITableChildren<TResponse> {
+	column(props: ITableColumnProps<TResponse>): ReactNode;
+}
+
+export interface ITableChildrenCallback<TResponse> {
+	(children: IITableChildren<TResponse>): ReactNode;
+}
+
+
+export interface ITableProps<TResponse> extends Omit<TableProps<TResponse>, "children"> {
+	children?: ITableChildrenCallback<TResponse> | ReactNode;
 	loading?: Partial<SpinProps>;
 	withLoading?: keyof Pick<UseQueryResult, "isLoading" | "isFetching" | "isRefetching">;
 }
 
-export const Table = <TSourceContext extends ISourceContext<any>>(
+export const Table = <TResponse, >(
 	{
 		children,
-		header,
-		footer,
 		loading,
 		withLoading = "isFetching",
 		...props
-	}: ITableProps<TSourceContext>) => {
+	}: ITableProps<TResponse>) => {
 	const {t} = useTranslation();
-	const sourceContext = useSourceContext() as TSourceContext;
-	if (header && !props.title) {
-		props.title = () => header(sourceContext);
-	}
+	const sourceContext = useSourceContext<TResponse>();
 	return <PaginationProvider>
 		<PaginationContext.Consumer>
 			{paginationContext =>
-				<CoolTable<ISourceItem<TSourceContext>>
+				<CoolTable<any>
 					style={{minHeight: "50vh"}}
 					showSorterTooltip={false}
 					dataSource={sourceContext.data()}
@@ -50,17 +56,15 @@ export const Table = <TSourceContext extends ISourceContext<any>>(
 						...paginationContext.pagination(),
 						position: ["bottomRight"],
 					}}
-					footer={() => footer?.(sourceContext)}
 					{...props}
 				>
-					{isCallable(children) ? (children as ITableChildrenCallback<TSourceContext>)({
+					{isCallable(children) ? (children as ITableChildrenCallback<any>)({
 						column: (props: any) => {
 							if (isString(props.title)) {
 								props.title = t(props.title as string);
 							}
 							return <CoolTable.Column {...props}/>;
 						},
-						sourceContext,
 					}) : children as any}
 				</CoolTable>}
 		</PaginationContext.Consumer>
