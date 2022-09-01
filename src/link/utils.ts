7a312a5c86d29e1e97ddc51e89edd8dbe457815e
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-01 13:36:24 +0200

    Common: Version bump

--- src/link/utils.ts
+++ src/link/utils.ts
@@ -1,8 +1,6 @@
 import {IQueryParams} from "@leight-core/api";
 import {compile} from "path-to-regexp";
 
-const generator = (path: string): (query: any) => string => compile(path.replace(/\[(.*?)\]/g, ":$1").replace(/{(.*?)}/g, ":$1"));
-
 export const toLink = <TQuery extends IQueryParams | void = void>(href: string, query?: TQuery): string => {
-	return href === "/" ? href : generator(href)(query);
+	return href === "/" ? href : compile(href.replace(/\[(.*?)\]/g, ":$1").replace(/{(.*?)}/g, ":$1"))(query || {});
 };
