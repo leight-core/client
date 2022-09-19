0dbb65687ab1e922bc4665dfd04393767dcc47d4
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-19 11:52:52 +0200

    Common: Version bump

--- src/source/utils.ts
+++ src/source/utils.ts
@@ -63,7 +63,7 @@ export function createQueryHook<TRequest, TResponse, TQueryParams extends IQuery
 
 export function createMutationHook<TRequest, TResponse, TQueryParams extends IQueryParams = any>(link: string, method: Method): IMutationHook<TRequest, TResponse, TQueryParams> {
 	return (query?, options?, config?) => {
-		return useMutation<TResponse, any, TRequest>(["mutation", link, {query}], request => toPromise<TRequest, TResponse>(method, toLink(link, query), request, config), options);
+		return useMutation<TResponse, any, TRequest>(["mutation", link, method, {query}], request => toPromise<TRequest, TResponse>(method, toLink(link, query), request, config), options);
 	};
 }
 
