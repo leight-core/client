315f7449672337e6aa32df3af7beef6047586e9f
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-25 18:08:35 +0200

    Common: Improved selection

--- src/selection/SelectionProvider.tsx
+++ src/selection/SelectionProvider.tsx
@@ -1,13 +1,12 @@
 import {ISelection, ISelectionContext, ISelectionType} from "@leight-core/api";
 import {SelectionContext} from "@leight-core/client";
-import {PropsWithChildren, useEffect, useRef, useState} from "react";
+import {PropsWithChildren, useEffect, useRef} from "react";
 
 export type ISelectionProviderProps<TSelection = any> = PropsWithChildren<{
 	/**
 	 * Selection type.
 	 */
 	type?: ISelectionType;
-	defaultEnabled?: boolean;
 	/**
 	 * Default pre-set selection; could be overridden by a user. Apply selection prop takes precedence.
 	 */
@@ -18,40 +17,37 @@ export type ISelectionProviderProps<TSelection = any> = PropsWithChildren<{
 	applySelection?: Record<string, TSelection>;
 	/**
 	 * Default selection handler.
-	 * @param event
+	 * @param selection
 	 */
-	onSelection?(event: ISelection<TSelection>): void;
+	onSelection?(selection: ISelection<TSelection>): void;
 }>;
 
-export function SelectionProvider<TSelection, >({type = "single", defaultEnabled = true, defaultSelection, applySelection, onSelection, ...props}: ISelectionProviderProps<TSelection>) {
-	const [enabled, setEnabled] = useState(defaultEnabled);
-	const [selection, setSelection] = useState<Record<string, TSelection | undefined>>({...defaultSelection, ...applySelection});
+export function SelectionProvider<TSelection, >({type = "single", defaultSelection, applySelection, onSelection, ...props}: ISelectionProviderProps<TSelection>) {
 	const onSelectionEvents = useRef<((event: ISelection<TSelection>) => void)[]>(onSelection ? [onSelection] : []);
+	const selectionRef = useRef<Record<string, TSelection>>({...defaultSelection, ...applySelection});
+	const setSelection: (callback: (prev: Record<string, TSelection>) => Record<string, TSelection>) => void = prev => selectionRef.current = prev(selectionRef.current);
 
 	useEffect(() => {
-		setSelection({...defaultSelection, ...applySelection});
+		setSelection(() => ({...defaultSelection, ...applySelection}));
 	}, [JSON.stringify(defaultSelection)]);
 	useEffect(() => {
-		setSelection({...defaultSelection, ...applySelection});
+		setSelection(() => ({...defaultSelection, ...applySelection}));
 	}, [JSON.stringify(applySelection)]);
 
 	const select: ISelectionContext<TSelection>["select"] = (id, $selection, select) => {
 		setSelection(prev => {
-			console.log("SelectionProvider: Current selection", selection);
 			const $select = select === undefined ? !prev[id] : select;
-			console.log("SelectionProvider: Selecting", type, "$select", $select, "item", $select ? {[id]: $selection} : {});
 			if (type === "single") {
 				return $select ? {[id]: $selection} : {};
 			}
-			console.log("SelectionProvider: Multi", {...prev, [id]: $select ? $selection : undefined});
-			return {...prev, [id]: $select ? $selection : undefined};
+			prev[id] = $selection;
+			!$select && (delete prev[id]);
+			return prev;
 		});
-		setTimeout(() => {
-			console.log("SelectionProvider: Selection timeout; selection", selection);
-		}, 100);
+		console.log("SelectionProvider: Selection timeout; selection", selectionRef.current);
 	};
-	const isSelected: ISelectionContext<TSelection>["isSelected"] = id => !!selection[id];
-	const toSelection: ISelectionContext<TSelection>["toSelection"] = () => Object.keys(selection).filter(key => !!selection[key]);
+	const isSelected: ISelectionContext<TSelection>["isSelected"] = id => !!selectionRef.current[id];
+	const toSelection: ISelectionContext<TSelection>["toSelection"] = () => Object.keys(selectionRef.current).filter(key => !!selectionRef.current[key]);
 	const isEmpty: ISelectionContext<TSelection>["isEmpty"] = () => !toSelection().length;
 	const toSingle: ISelectionContext<TSelection>["toSingle"] = () => {
 		if (isEmpty()) {
@@ -60,12 +56,12 @@ export function SelectionProvider<TSelection, >({type = "single", defaultEnabled
 		return toSelection()[0];
 	};
 	const $selection = () => {
-		const items = toSelection().reduce((prev, current) => ({...prev, [current]: selection[current]}), {});
+		const items = toSelection().reduce((prev, current) => ({...prev, [current]: selectionRef.current[current]}), {});
 		return {
 			isEmpty: isEmpty(),
 			single: (() => {
 				try {
-					return selection[toSingle()];
+					return selectionRef.current[toSingle()];
 				} catch (e) {
 					// swallow "Selection Empty" error
 				}
@@ -78,15 +74,10 @@ export function SelectionProvider<TSelection, >({type = "single", defaultEnabled
 
 	return <SelectionContext.Provider
 		value={{
-			enable: (enable = true) => {
-				setSelection({});
-				setEnabled(enable);
-			},
-			isEnabled: () => enabled,
 			isSelected,
-			asSelection: () => selection,
+			asSelection: () => selectionRef.current,
 			toSelection,
-			toItems: () => Object.values(selection).filter(item => !!item),
+			toItems: () => Object.values(selectionRef.current).filter(item => !!item),
 			select,
 			item: (item, $select) => select(item.id, item, $select),
 			items: (items, $select) => items.map(item => select(item.id, item, $select)),
@@ -95,15 +86,12 @@ export function SelectionProvider<TSelection, >({type = "single", defaultEnabled
 			toSingle,
 			onSelection: callback => onSelectionEvents.current.push(callback),
 			selection: $selection,
-			toSingleItem: () => selection[toSingle()],
+			toSingleItem: () => selectionRef.current[toSingle()],
 			handleSelection: () => {
 				const selection = $selection();
 				onSelectionEvents.current.map(callback => callback(selection));
 			},
-			clear: () => {
-				console.log("Calling clear??");
-				setSelection({});
-			},
+			clear: () => setSelection(() => ({})),
 		}}
 		{...props}
 	/>;
