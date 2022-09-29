b52d74e23d9fc8d93aeb3bca6bfd133f39f70c24
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-29 02:20:53 +0200

    Common: Version bump

--- src/list/InfiniteList.tsx
+++ src/list/InfiniteList.tsx
@@ -1,21 +1,42 @@
-import Icon from "@ant-design/icons";
-import {IFilterContext, INavigate, ISourceContext} from "@leight-core/api";
-import {FulltextBar, useNavigate, useOptionalFilterContext, useSourceContext} from "@leight-core/client";
-import {DotLoading, InfiniteScroll, List, Space} from "antd-mobile";
+import {ICursorContext, IFilterContext, INavigate, ISourceContext, ITranslationProps} from "@leight-core/api";
+import {FulltextBar, Translate, useNavigate, useOptionalCursorContext, useOptionalFilterContext, useSourceContext} from "@leight-core/client";
+import {toPercent} from "@leight-core/utils";
+import {Col, Row} from "antd";
+import {DotLoading, ErrorBlock, InfiniteScroll, List, ProgressCircle} from "antd-mobile";
 import {ComponentProps, FC, ReactNode} from "react";
-import {IoTrailSignOutline} from "react-icons/io5";
 
 export interface IInfiniteListHeaderRequest<TResponse> {
 	sourceContext: ISourceContext<TResponse>;
 	filterContext?: IFilterContext | null;
 }
 
+export interface IInfiniteListSelectRenderLoading<TResponse> {
+	sourceContext: ISourceContext<TResponse>;
+	cursorContext: ICursorContext | null;
+}
+
+export interface IInfiniteListSelectRenderEmpty<TResponse> {
+	sourceContext: ISourceContext<TResponse>;
+	cursorContext: ICursorContext | null;
+}
+
 export interface IInfiniteListProps<TResponse> extends Partial<Omit<ComponentProps<typeof List>, "children" | "header">> {
+	translation?: ITranslationProps;
+
 	children?(item: TResponse): ReactNode;
 
 	header?(request: IInfiniteListHeaderRequest<TResponse>): ReactNode;
 
 	withFulltext?: boolean;
+
+	/**
+	 * Override loading state indicator including no more data/empty list.
+	 *
+	 * @param props
+	 */
+	renderLoading?(props: IInfiniteListSelectRenderLoading<TResponse>): ReactNode;
+
+	renderEmpty?(props: IInfiniteListSelectRenderEmpty<TResponse>): ReactNode;
 }
 
 export interface IInfiniteListItemProps extends Omit<ComponentProps<typeof List["Item"]>, "onClick"> {
@@ -34,11 +55,15 @@ export const InfiniteListItem: FC<IInfiniteListItemProps> = ({onClick, clickable
 export const InfiniteList = <TResponse, >(
 	{
 		children,
+		translation,
 		withFulltext = false,
+		renderLoading,
+		renderEmpty,
 		header,
 		...props
 	}: IInfiniteListProps<TResponse>) => {
 	const sourceContext = useSourceContext<TResponse>();
+	const cursorContext = useOptionalCursorContext();
 	const filterContext = useOptionalFilterContext();
 	if (withFulltext && header) {
 		console.warn(`Using infinite list ${sourceContext.name} with fulltext and header specified; please use flags only or just header.`);
@@ -54,13 +79,24 @@ export const InfiniteList = <TResponse, >(
 			loadMore={async () => sourceContext.more(true)}
 			hasMore={sourceContext.hasMore()}
 		>
-			<Space>
-				{sourceContext.result.isFetching || sourceContext.hasMore() ? (
-					<DotLoading/>
-				) : (
-					<Icon component={IoTrailSignOutline} size={128}/>
-				)}
-			</Space>
+			{renderLoading?.({
+				sourceContext,
+				cursorContext,
+			}) || (cursorContext?.page === undefined || cursorContext?.pages === undefined ?
+				<DotLoading/> : (cursorContext.pages > 0 ? <Row align={"top"} justify={"center"} gutter={4}>
+					<Col span={"auto"}>{`${cursorContext?.page}/${cursorContext?.pages}`}</Col>
+					{cursorContext.page !== cursorContext.pages && <Col span={2}><ProgressCircle
+						percent={toPercent(cursorContext?.page || 0, cursorContext?.pages || 0)}
+						style={{"--size": "18px", "--track-width": "2px"}}
+					/></Col>}
+				</Row> : renderEmpty?.({
+					sourceContext,
+					cursorContext,
+				}) || <ErrorBlock
+					status={"empty"}
+					title={<Translate {...translation} text={"empty.title"}/>}
+					description={<Translate {...translation} text={"empty.description"}/>}
+				/>))}
 		</InfiniteScroll>
 	</>;
 };
