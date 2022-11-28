f4af681f308388c190e662970d341eeffd2ed568
Marek Hanzal <marek.hanzal@x32.cz>
2022-11-28 11:28:53 +0100

    Common: Version bump

--- src/date/utils.ts
+++ src/date/utils.ts
@@ -3,10 +3,6 @@ import {
 	Duration,
 	DurationUnitType
 }                          from "dayjs/plugin/duration";
-import moment, {
-	Moment,
-	MomentInput
-}                          from "moment";
 
 export const toLocalDate = (input?: ConfigType | null, fallback = "-"): string => {
 	return input ? dayjs(input).format("L") : fallback;
@@ -27,10 +23,6 @@ export const toUtcDateTime = (input ?: ConfigType | null, fallback: string | nul
 
 export const asDayjs = (input?: ConfigType | null, fallback: ConfigType | null = null) => {
 	return input ? dayjs(input) : (fallback ? dayjs(fallback) : null);
-};
-
-export const asMoment = (input?: MomentInput, fallback: MomentInput | null = null): Moment | null => {
-	return input ? moment(input) : (fallback ? moment(fallback) : null);
 };
 
 export const durationOf = (date: ConfigType, from?: ConfigType, unit?: DurationUnitType): Duration => {
